6b416fd63ea0c43eaaff6f82e0d9014bbadc7a53
Jack <mrzzcn@gmail.com>
2015-07-31 09:56:19 +0800

    Update functions.js
    可以不以this 开始调用属性

--- functions.js
+++ functions.js
@@ -1,15 +1,17 @@
 var TemplateEngine = function(html, options) {
-    var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
+    var re = /\{\{([^\}\}]+)?\}\}/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
     var add = function(line, js) {
         js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
             (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
         return add;
     }
     while(match = re.exec(html)) {
-        add(html.slice(cursor, match.index))(match[1], true);
+        var prop = match[1].replace(/\ /ig,'');
+        prop = prop.indexOf("this.") === 0 ? prop : "this." + prop;
+        add(html.slice(cursor, match.index))(prop, true);
         cursor = match.index + match[0].length;
     }
-    add(html.substr(cursor, html.length - cursor));
+    add(html.substr(cursor, html.length - cursor)); console.log(code);
     code += 'return r.join("");';
     return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
 }

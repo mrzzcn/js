f4d72f3f54277b6f1b2cbd0edcc2b70f3f277982
Jack <mrzzcn@gmail.com>
2015-08-08 18:28:16 +0800

    Update rootless.sh

--- rootless.sh
+++ rootless.sh
@@ -1,6 +1,7 @@
 ###mac 无法复制文件到USR目录：
 #http://tadaland.com/os-x-rootless.html
 sudo nvram boot-args="rootless=0" 
+sudo nvram boot-args="kext-dev-mode=1 rootless=0";sudo reboot
 
 #修改完成之后恢复配置：
 sudo nvram -d boot-args && sudo reboot
